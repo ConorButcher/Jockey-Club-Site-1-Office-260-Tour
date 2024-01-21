c0a4452c12dbc34ad0e5561985027ad63b4416b6
Conor Butcher <conorbutcher22@outlook.com>
2024-01-21 12:17:42 +0000

    Initial Tour Commit

---
+++ data.js
@@ -0,0 +1,431 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-site-1-office-360-camera-001",
+      "name": "Site 1 Office 360 Camera 001",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -2.3228033446535967,
+        "pitch": 0.015700828492672514,
+        "fov": 1.490377985184405
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2539216695316373,
+          "pitch": 0.017159945185074577,
+          "rotation": 0,
+          "target": "1-site-1-office-360-camera-002"
+        },
+        {
+          "yaw": -2.190343730805779,
+          "pitch": 0.0342920981124184,
+          "rotation": 7.853981633974483,
+          "target": "2-site-1-office-360-camera-003"
+        },
+        {
+          "yaw": -2.407943507617107,
+          "pitch": 0.04148260443299634,
+          "rotation": 0,
+          "target": "3-site-1-office-360-camera-004"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-site-1-office-360-camera-002",
+      "name": "Site 1 Office 360 Camera 002",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 1.5633601397105963,
+        "pitch": 0.05031846785976235,
+        "fov": 1.490377985184405
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.563360139449932,
+          "pitch": 0.021458960687535722,
+          "rotation": 0,
+          "target": "0-site-1-office-360-camera-001"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-site-1-office-360-camera-003",
+      "name": "Site 1 Office 360 Camera 003",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 1.5797228931728844,
+        "pitch": 0.017064356964995753,
+        "fov": 1.490377985184405
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.458216469415527,
+          "pitch": -0.0028966715639331397,
+          "rotation": 0,
+          "target": "3-site-1-office-360-camera-004"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-site-1-office-360-camera-004",
+      "name": "Site 1 Office 360 Camera 004",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.6954465788715893,
+        "pitch": 0.017934753130981917,
+        "fov": 1.490377985184405
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4224771035088644,
+          "pitch": -0.0035758613388523486,
+          "rotation": 0,
+          "target": "2-site-1-office-360-camera-003"
+        },
+        {
+          "yaw": -0.14769546114042242,
+          "pitch": 0.02196049048550819,
+          "rotation": 4.71238898038469,
+          "target": "1-site-1-office-360-camera-002"
+        },
+        {
+          "yaw": 0.5196259585699572,
+          "pitch": 0.02467041095344058,
+          "rotation": 0,
+          "target": "0-site-1-office-360-camera-001"
+        },
+        {
+          "yaw": 3.0601127460572304,
+          "pitch": -0.013285388415122057,
+          "rotation": 0,
+          "target": "4-site-1-office-360-camera-005"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-site-1-office-360-camera-005",
+      "name": "Site 1 Office 360 Camera 005",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 2.255315751731745,
+        "pitch": -0.018097796341262296,
+        "fov": 1.490377985184405
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.09440702035712079,
+          "pitch": 0.024018588971813415,
+          "rotation": 0,
+          "target": "3-site-1-office-360-camera-004"
+        },
+        {
+          "yaw": -1.5044618936269902,
+          "pitch": 0.0010803528756184733,
+          "rotation": 0,
+          "target": "5-site-1-office-360-camera-006"
+        },
+        {
+          "yaw": 2.3478932941329003,
+          "pitch": -0.009200405408478218,
+          "rotation": 0,
+          "target": "6-site-1-office-360-camera-007"
+        },
+        {
+          "yaw": -3.0780943095367945,
+          "pitch": 0.005117574952723913,
+          "rotation": 0,
+          "target": "7-site-1-office-360-camera-008"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-site-1-office-360-camera-006",
+      "name": "Site 1 Office 360 Camera 006",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 1.5652597785329085,
+        "pitch": 0.0978478877272213,
+        "fov": 1.490377985184405
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4780149171440824,
+          "pitch": 0.01836860653944683,
+          "rotation": 0,
+          "target": "4-site-1-office-360-camera-005"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-site-1-office-360-camera-007",
+      "name": "Site 1 Office 360 Camera 007",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -0.5840158858149653,
+        "pitch": -0.046630358140546946,
+        "fov": 1.490377985184405
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6737832028009816,
+          "pitch": 0.02941328732121562,
+          "rotation": 0,
+          "target": "4-site-1-office-360-camera-005"
+        },
+        {
+          "yaw": -0.9510749223957227,
+          "pitch": 0.029619504143138187,
+          "rotation": 4.71238898038469,
+          "target": "5-site-1-office-360-camera-006"
+        },
+        {
+          "yaw": -1.9783746396612507,
+          "pitch": 0.03240666549354465,
+          "rotation": 0,
+          "target": "7-site-1-office-360-camera-008"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-site-1-office-360-camera-008",
+      "name": "Site 1 Office 360 Camera 008",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.4727043077171196,
+        "pitch": -0.025741645670350977,
+        "fov": 1.490377985184405
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0940276914751763,
+          "pitch": 0.056522711811201276,
+          "rotation": 0,
+          "target": "6-site-1-office-360-camera-007"
+        },
+        {
+          "yaw": -0.049814693365194174,
+          "pitch": 0.02655726669651237,
+          "rotation": 0,
+          "target": "4-site-1-office-360-camera-005"
+        },
+        {
+          "yaw": -0.23409488734535877,
+          "pitch": 0.02134786399803268,
+          "rotation": 4.71238898038469,
+          "target": "5-site-1-office-360-camera-006"
+        },
+        {
+          "yaw": -1.5475253535431914,
+          "pitch": 0.008425514273298518,
+          "rotation": 0,
+          "target": "8-site-1-office-360-camera-009"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-site-1-office-360-camera-009",
+      "name": "Site 1 Office 360 Camera 009",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 2.2686028713726127,
+        "pitch": -0.01542136748841827,
+        "fov": 1.490377985184405
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.042833875234871,
+          "pitch": 0.008995791949994114,
+          "rotation": 0,
+          "target": "7-site-1-office-360-camera-008"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
